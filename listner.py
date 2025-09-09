926963d71cec319036cc64d19e953cafa92baec2
Sanjeev <miuractech@gmail.com>
2025-09-09 05:47:47 +0530

    test

--- listner.py
+++ listner.py
@@ -10,8 +10,8 @@ WATCHED_DIR = "/home/sanjeevexpo1/test"
 def on_new_folder(folder_path):
     print(f"New folder created: {folder_path}")
     # You can run your actual script here
-    script_path = "/opt/imagepick/src/upload_manager.py"
-    os.system(f"sudo python3 {script_path} \"{folder_path}\"")
+    script_path = "/opt/imagepick/test_execute.py"
+    os.system(f"sudo python3 {script_path}")
     # Or call a function directly
 
 class FolderCreationHandler(FileSystemEventHandler):
